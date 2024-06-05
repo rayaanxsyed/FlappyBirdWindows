9cd33ec3ec1aa458a0c50ff71992068d55c0c471
Rayaan <rayaanksyed@gmail.com>
2024-06-04 23:16:01 -0400

    First Initial Windows Download Commit

---
+++ main.lua
@@ -0,0 +1,106 @@
+push = require 'push'
+Class = require 'class'
+require 'Bird'
+
+require 'Pipe'
+
+WINDOW_WIDTH = 1280 
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512 
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
+local bird = love.graphics.newImage('ground.png')
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local backgroundScroll = 0 
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local bird = Bird()
+
+local pipes = {}
+
+local spawnTimer = 0
+
+
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Flappy Bird')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true, 
+        fullscreen = false,
+        resizable = true
+    })
+
+    math.randomseed(os.time())
+
+    love.keyboard.keysPressed = {}
+end 
+
+function love.resize(w,h)
+    push:resize(w,h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+    if key == 'escape' then
+        love.event.quit()
+    end 
+end 
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then 
+        return true
+    else 
+        return false
+    end
+end
+
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
+        
+    spawnTimer = spawnTimer + dt
+    
+    if spawnTimer > 2 then 
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+
+    bird:update(dt)
+
+    for k, pipe in pairs(pipes) do 
+        pipe:update(dt)
+
+        if pipe.x < -pipe.width then 
+            table.remove(pipes, k)
+        end
+    end
+    
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
+    bird:render()
+
+    push:finish()
+end
